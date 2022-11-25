45971ef4ffab81036b0d84cb35a9943ad7511a95
ZawYeWint <zawyewint@my.jcu.edu.au>
2022-11-25 16:24:55 +0800

    Fixing Errors

---
+++ DebuggingExercises/FixDebugTwo2.java
@@ -0,0 +1,16 @@
+public class FixDebugTwo2
+//  This application performs arithmetic with two integers
+{
+   public static void main(String args[])
+   {
+      int a, b;
+      a = 7;
+      b = 4;
+      int sum = a+b;
+      int difference = a-b;
+      int product = a*b;
+      System.out.println("The sum is " + sum);
+      System.out.println("The difference is " + difference);
+      System.out.println("The product is " + product);
+   }
+}
